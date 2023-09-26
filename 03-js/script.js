5ef5582c0f69401326aa8f7911891d03329a51fb
auzanassdq <auzanassidqi@gmail.com>
2023-09-26 13:31:34 +0700

    js dasar

---
+++ 03-js/script.js
@@ -0,0 +1,107 @@
+console.log("ini dari external JS");
+
+// =============== tipe data =================
+// // string
+// "hallo ini saya"
+// 'auzan'
+// `halloooo`
+// "20"
+
+// // number
+// 10
+// 9
+// 3.14
+// -2
+
+// // boolean
+// true
+// false
+// 1 == 1
+// !true
+// 20 < 100
+// true && true
+
+// // array
+// [1,2,3,4]
+// ["kucing", "kambing"]
+
+// // object
+// let kucing = {
+//   nama: "tom",
+//   warna: "biru"
+// }
+
+
+// =============== variabel =================
+// // var
+// // let
+// // const
+// let nama = "alpha"
+// const NIK = "312411556"
+
+// console.log(nama);
+// console.log(NIK);
+
+// nama = "delta"
+// // NIK = "4234324"
+
+// console.log(nama);
+
+// let P = 5
+// let L = 2
+// let luas = P * L
+
+// console.log(luas);
+
+// ================== Operator ================
+// + - * / =
+// %
+// ==
+// ===
+// &&
+// ||
+// !
+// conditon ? true : false
+
+// console.log(1 + 1);
+// console.log("Hallo" + " " + "aplha");
+
+// let nama = "Terra"
+// console.log("hallo " + nama);
+// console.log(2 == "2");
+// console.log(2 === "2");
+
+
+// ================== conditional ================
+// if
+// switch
+// ternary operator
+
+let uang = 20_000
+
+if (uang > 50_000) {
+  console.log("ngajak jalan");
+} else {
+  console.log("minggu depan aja jalannya");
+}
+
+// ================== loop ================
+// for
+// while
+// do while
+
+// ================== function ================
+// function
+
+function cekUangSebelumJalan(uang){
+  if (uang > 50_000) {
+    console.log("ngajak jalan");
+  } else {
+    console.log("minggu depan aja jalannya");
+  }
+}
+
+cekUangSebelumJalan(10_000)
+
+cekUangSebelumJalan(80_000)
+
